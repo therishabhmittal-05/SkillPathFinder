254d249cab58df8427b92098543185426fd2587d
Mehak Singla <singlamehak2005@gmail.com>
2024-07-29 09:52:31 -0700

    done with the setup of the site

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('MERN Stack API');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
