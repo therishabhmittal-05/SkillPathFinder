86f180b299c1016d97c67cf07a45ba6e29998b92
Mehak Singla <singlamehak2005@gmail.com>
2024-08-03 00:43:50 +0530

    Revert "Build 2"
    This reverts commit fc98e41a4b79122d627e8dc8a3903a9679cab5bd.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,30 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// // Middleware
+// app.use(express.json());
+
+// // Connect to MongoDB
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log('MongoDB connected'))
+// .catch(err => console.log(err));
+
+// // Routes
+// app.get('/', (req, res) => {
+//   res.send('MERN Stack API');
+// });
+
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 import express from "express";
 import dotenv from "dotenv";
-
-
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/recommendation.routes.js";
@@ -15,12 +38,12 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
-console.log(__dirname)
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
-const PORT =  process.env.PORT2 || 8000;
+const PORT =  8000;
 
 app.use(express.json()); 
 app.use(cookieParser());
@@ -37,21 +60,11 @@ app.use("/api/userData",userRouter);
 
 
 app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req, res)=>{
+	res.sendfile(path.join(__dirname,"/client/dist/index.html"))
+})
 
-// app.get('*',(req, res)=>{
-// 	res.sendfile(path.join(__dirname,"/client/dist/index.html"))
-// })
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname,'/client','/dist','/index.html'));
-  })
-console.log(__dirname,'/client','/dist','/index.html')
 app.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 });
-
-
-
-
-
-
