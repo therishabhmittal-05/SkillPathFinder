bf8ef4bfeb3aba6026db8e7996b0f0d8124d4e21
Mehak Singla <singlamehak2005@gmail.com>
2024-08-02 23:26:53 +0530

    its proxy

--- server/server.js
+++ server/server.js
@@ -66,6 +66,9 @@
 // app.get('*',(req, res)=>{
 // 	res.sendfile(path.join(__dirname,"/client/dist/index.html"))
 // })
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+//   })
 
 // app.listen(PORT, () => {
 // 	connectToMongoDB();
