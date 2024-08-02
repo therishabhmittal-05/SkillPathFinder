bdad58f0c44fba70339d0531d0d5d442a35b4914
Mehak Singla <singlamehak2005@gmail.com>
2024-08-02 18:38:42 +0530

    Build done

--- server/server.js
+++ server/server.js
@@ -33,6 +33,11 @@ import cookieParser from 'cookie-parser'
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import userRouter from "./routes/user.routes.js";
+import path from 'path';
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
 
 dotenv.config();
 
@@ -43,10 +48,21 @@ const PORT =  8000;
 app.use(express.json()); 
 app.use(cookieParser());
 
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/user",userRoutes);
 app.use("/api/userData",userRouter);
 
+
+
+
+
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req, res)=>{
+	res.sendfile(path.join(__dirname,"/client/dist/index.html"))
+})
 
 app.listen(PORT, () => {
 	connectToMongoDB();
